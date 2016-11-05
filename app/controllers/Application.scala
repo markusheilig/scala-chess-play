cb3658b164629f836411967f802e5b70c591e487
Markus Heilig <markusheilig@gmx.net>
2016-11-05 11:24:06 +0100

    create connection to remote actor

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,14 +2,26 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api._
-import play.api.mvc._
 import akka.actor._
 import akka.stream.Materializer
 import play.api.libs.json.JsValue
 import play.api.libs.streams.ActorFlow
+import play.api.mvc._
+
+class LocalController extends Actor {
+
+  val remoteController = context.actorSelection("akka.tcp://chess@127.0.0.1:2552/user/controller")
+
+  override def receive: Receive = {
+    case "START" => remoteController ! "hi from scala-chess-play"
+    case msg: String => println(s"received message '$msg'")
+  }
+}
 
 class Application @Inject() (implicit system: ActorSystem, materializer: Materializer) extends Controller {
+
+  val localController = system.actorOf(Props[LocalController], name = "remoteController")
+  localController ! "START"
 
   def socket = WebSocket.accept[JsValue, JsValue] { request =>
     ActorFlow.actorRef(out => MyWebSocketActor.props(out))
