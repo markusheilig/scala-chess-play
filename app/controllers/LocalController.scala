bc0af988d447fbdf43ab0140911a7cf23637a30a
Markus Heilig <markusheilig@gmx.net>
2016-11-05 13:47:37 +0100

    rename 'socket' to 'observer'

--- app/controllers/LocalController.scala
+++ app/controllers/LocalController.scala
@@ -1,21 +1,24 @@
 package controllers
 
 import akka.actor.{Actor, ActorRef}
+import chess.api.{Castle, Move}
 
-case class AppendSocket(actorRef: ActorRef)
-case class RemoveSocket(actorRef: ActorRef)
+case class AppendObserver(observer: ActorRef)
+case class RemoveObserver(observer: ActorRef)
 
 class LocalController extends Actor {
 
   val remoteController = context.actorSelection("akka.tcp://chess@127.0.0.1:2552/user/controller")
-  val outSockets = scala.collection.mutable.ListBuffer.empty[ActorRef]
+  val observers = scala.collection.mutable.ListBuffer.empty[ActorRef]
 
   override def receive: Receive = {
-    case AppendSocket(socket) => outSockets += socket
-    case RemoveSocket(socket) => outSockets -= socket
+    case AppendObserver(observer) => observers += observer
+    case RemoveObserver(observer) => observers -= observer
+    case m: Move => remoteController ! m
+    case c: Castle => remoteController ! c
     case "START" => remoteController ! "hi from scala-chess-play"
     case msg: String =>
       println(s"received message '$msg'")
-      outSockets.foreach(_ ! s"$msg")
+      observers.foreach(_ ! s"$msg")
   }
 }
